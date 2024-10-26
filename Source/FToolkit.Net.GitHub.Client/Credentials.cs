3a4191e38e119b91badefcd9761bffcea06a992c
finphie <19237170+finphie@users.noreply.github.com>
2024-10-27 08:51:18 +0900

    Update Credentials.cs

--- Source/FToolkit.Net.GitHub.Client/Credentials.cs
+++ Source/FToolkit.Net.GitHub.Client/Credentials.cs
@@ -3,24 +3,16 @@
 /// <summary>
 /// GitHub APIの認証に必要な資格情報。
 /// </summary>
-public sealed record Credentials
+/// <param name="Type">認証の種類。</param>
+/// <param name="Token">GitHub APIのトークン。</param>
+public sealed record Credentials(AuthenticationType Type, string Token)
 {
-    /// <summary>
-    /// 認証の種類。
-    /// </summary>
-    public AuthenticationType Type { get; init; }
-
-    /// <summary>
-    /// GitHub APIのトークン。
-    /// </summary>
-    public required string Token { get; init; }
-
     /// <summary>
     /// <see cref="AuthenticationType.Token"/>に設定した資格情報を返します。
     /// </summary>
     /// <param name="token">GitHub APIのトークン</param>
     public static implicit operator Credentials(string token)
-        => new() { Type = AuthenticationType.Token, Token = token };
+        => new(AuthenticationType.Token, token);
 
     /// <inheritdoc/>
     public override string ToString()
