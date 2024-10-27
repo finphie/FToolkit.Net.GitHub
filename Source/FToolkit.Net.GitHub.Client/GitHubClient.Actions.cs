43c718e8fe6d882216d2c4762673f75dbc06c6d7
finphie <19237170+finphie@users.noreply.github.com>
2024-10-27 10:53:45 +0900

    Update GitHubClient.Actions.cs

--- Source/FToolkit.Net.GitHub.Client/GitHubClient.Actions.cs
+++ Source/FToolkit.Net.GitHub.Client/GitHubClient.Actions.cs
@@ -3,6 +3,9 @@ using FToolkit.Net.GitHub.Client.Actions.Permissions;
 
 namespace FToolkit.Net.GitHub.Client;
 
+/// <summary>
+/// <see cref="IActionsClient"/>を実装する部分クラスです。
+/// </summary>
 partial class GitHubClient : IActionsClient
 {
     /// <inheritdoc/>
